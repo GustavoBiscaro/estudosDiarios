4baaa169fafb48a4766006671c68ea4628b1990f
unknown <Administrador@TEC-01.kg2.local>
2024-08-05 17:27:33 -0300

    aula 1

---
+++ 1. REACT com VITE/project-test/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    
+      <div className='App'>
+       <h1>Front  Beginners</h1>
+      </div>
+
+  
+  )
+}
+
+export default App
