e715622c365c0987dc43457a306038bf25f3d544
unknown <Administrador@TEC-01.kg2.local>
2024-08-08 17:36:42 -0300

    one more lesson

---
+++ 4. useState/estados/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  let count = 0
+
+  return (
+ 
+      <div>
+        <h1>Front Beginners</h1>
+        <h3>{count}</h3>
+        <button>Add</button>
+      </div>
+      
+  )
+}
+
+export default App
